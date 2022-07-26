f5b4965b8ef2eacb846f1966ac2f8211acfcb874
firefoxic <firefoxic.dev@gmail.com>
2022-07-26 23:10:05 +0300

    Update project identic

--- test/index.js
+++ test/index.js
@@ -1,41 +1,41 @@
 const assert = require('assert').strict
 const fs = require('fs')
 const path = require('path')
-const htmlBemValidator1 = require('../src')
+const htmlBemValidator = require('../src')
 
 describe('gulp-html-bem-validator', () => {
 	describe('should work in buffer mode', () => {
 		it('should not be errors because a valid bem html is passed', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/valid.html'))
-			const { countBemWarning } = htmlBemValidator1.htmlBemValidator({ content: html.toString() })
+			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
 
 			assert.equal(countBemWarning, 0)
 		})
 
 		it('should be one error bem', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/no-valid-error-1.html'))
-			const { countBemWarning } = htmlBemValidator1.htmlBemValidator({ content: html.toString() })
+			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
 
 			assert.strictEqual(countBemWarning, 1)
 		})
 
 		it('should be one error bem(prefix)', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/prefix-error.html'))
-			const { countBemWarning } = htmlBemValidator1.htmlBemValidator({ content: html.toString() })
+			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
 
 			assert.strictEqual(countBemWarning, 1)
 		})
 
 		it('should be one error element-within-an-element', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/element-within-an-element.html'))
-			const { countBemWarning } = htmlBemValidator1.htmlBemValidator({ content: html.toString() })
+			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
 
 			assert.strictEqual(countBemWarning, 1)
 		})
 
 		it('should not be errors because a valid bem html is passed', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/page__elements.html'))
-			const { countBemWarning } = htmlBemValidator1.htmlBemValidator({ content: html.toString() })
+			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
 
 			assert.strictEqual(countBemWarning, 0)
 		})
@@ -43,25 +43,25 @@ describe('gulp-html-bem-validator', () => {
 		it('output the success result to the console', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/valid.html'))
 
-			htmlBemValidator1.htmlBemValidatorResult({ name: 'valid.html', content: html.toString() })
+			htmlBemValidator.htmlBemlinterResult({ name: 'valid.html', content: html.toString() })
 		})
 
 		it('output the error result to the console', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/no-valid-error-1.html'))
 
-			htmlBemValidator1.htmlBemValidatorResult({ name: 'no-valid-error-1.html', content: html.toString() })
+			htmlBemValidator.htmlBemlinterResult({ name: 'no-valid-error-1.html', content: html.toString() })
 		})
 
 		it('output the error result to the console', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/element-within-an-element.html'))
 
-			htmlBemValidator1.htmlBemValidatorResult({ name: 'element-within-an-element.html', content: html.toString() })
+			htmlBemValidator.htmlBemlinterResult({ name: 'element-within-an-element.html', content: html.toString() })
 		})
 
 		it('output the error result to the console', () => {
 			const html = fs.readFileSync(path.join(__dirname, '/fixtures/page__elements.html'))
 
-			htmlBemValidator1.htmlBemValidatorResult({ name: 'page__elements.html', content: html.toString() })
+			htmlBemValidator.htmlBemlinterResult({ name: 'page__elements.html', content: html.toString() })
 		})
 	})
 })
