9df5092af4f8b6668cdb1e047110cd5e5967e852
firefoxic <firefoxic.dev@gmail.com>
2022-08-11 07:22:06 +0300

    Update all dependencies
    To update Chalk to v5+, you need to transfer the project from cjs to esm.

--- test/index.js
+++ test/index.js
@@ -1,61 +1,65 @@
-const assert = require('assert').strict
-const fs = require('fs')
-const path = require('path')
-const htmlBemValidator = require('../src')
+import { htmlBemlinter, htmlBemlinterResult } from '../src/index.js'
+import assert from 'assert'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 describe('gulp-html-bemlinter', () => {
 	describe('should work in buffer mode', () => {
 		it('should not be errors because a valid bem html is passed', () => {
-			const html = fs.readFileSync(path.join(__dirname, '/fixtures/valid.html'))
-			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
+			const html = fs.readFileSync(join(__dirname, '/fixtures/valid.html'))
+			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
 
 			assert.equal(countBemWarning, 0)
 		})
 
 		it('should be one error bem', () => {
-			const html = fs.readFileSync(path.join(__dirname, '/fixtures/no-valid-error-1.html'))
-			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
+			const html = fs.readFileSync(join(__dirname, '/fixtures/no-valid-error-1.html'))
+			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
 
 			assert.strictEqual(countBemWarning, 1)
 		})
 
 		it('should be one error bem(prefix)', () => {
-			const html = fs.readFileSync(path.join(__dirname, '/fixtures/prefix-error.html'))
-			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
+			const html = fs.readFileSync(join(__dirname, '/fixtures/prefix-error.html'))
+			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
 
 			assert.strictEqual(countBemWarning, 1)
 		})
 
 		it('should be one error element-within-an-element', () => {
-			const html = fs.readFileSync(path.join(__dirname, '/fixtures/element-within-an-element.html'))
-			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
+			const html = fs.readFileSync(join(__dirname, '/fixtures/element-within-an-element.html'))
+			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
 
 			assert.strictEqual(countBemWarning, 1)
 		})
 
 		it('should not be errors because a valid bem html is passed', () => {
-			const html = fs.readFileSync(path.join(__dirname, '/fixtures/page__elements.html'))
-			const { countBemWarning } = htmlBemValidator.htmlBemlinter({ content: html.toString() })
+			const html = fs.readFileSync(join(__dirname, '/fixtures/page__elements.html'))
+			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
 
 			assert.strictEqual(countBemWarning, 0)
 		})
 
 		it('output the success result to the console', () => {
-			const html = fs.readFileSync(path.join(__dirname, '/fixtures/valid.html'))
+			const html = fs.readFileSync(join(__dirname, '/fixtures/valid.html'))
 
-			htmlBemValidator.htmlBemlinterResult({ name: 'valid.html', content: html.toString() })
+			htmlBemlinterResult({ name: 'valid.html', content: html.toString() })
 		})
 
 		it('output the error result to the console', () => {
-			const html = fs.readFileSync(path.join(__dirname, '/fixtures/no-valid-error-1.html'))
+			const html = fs.readFileSync(join(__dirname, '/fixtures/no-valid-error-1.html'))
 
-			htmlBemValidator.htmlBemlinterResult({ name: 'no-valid-error-1.html', content: html.toString() })
+			htmlBemlinterResult({ name: 'no-valid-error-1.html', content: html.toString() })
 		})
 
 		it('output the error result to the console', () => {
-			const html = fs.readFileSync(path.join(__dirname, '/fixtures/element-within-an-element.html'))
+			const html = fs.readFileSync(join(__dirname, '/fixtures/element-within-an-element.html'))
 
-			htmlBemValidator.htmlBemlinterResult({ name: 'element-within-an-element.html', content: html.toString() })
+			htmlBemlinterResult({ name: 'element-within-an-element.html', content: html.toString() })
 		})
 	})
 })
