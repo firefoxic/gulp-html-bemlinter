1b51f153a18004e6c0aca9f1dd5cb2e155f1da07
firefoxic <firefoxic.dev@gmail.com>
2022-08-12 17:59:39 +0300

    Add eslint to project

--- test/index.js
+++ test/index.js
@@ -1,65 +1,66 @@
-import { htmlBemlinter, htmlBemlinterResult } from '../src/index.js'
-import assert from 'assert'
-import fs from 'fs'
-import { fileURLToPath } from 'url'
-import { join, dirname } from 'path'
+import { htmlBemlinter, htmlBemlinterResult } from "../src/index.js"
+import assert from "assert"
+import fs from "fs"
+import { fileURLToPath } from "url"
+import { join, dirname } from "path"
+import { describe, it } from "mocha"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+let __filename = fileURLToPath(import.meta.url)
+let __dirname = dirname(__filename)
 
-describe('gulp-html-bemlinter', () => {
-	describe('should work in buffer mode', () => {
-		it('should not be errors because a valid bem html is passed', () => {
-			const html = fs.readFileSync(join(__dirname, '/fixtures/valid.html'))
-			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
+describe("gulp-html-bemlinter", () => {
+	describe("should work in buffer mode", () => {
+		it("should not be errors because a valid bem html is passed", () => {
+			let html = fs.readFileSync(join(__dirname, "/fixtures/valid.html"))
+			let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			assert.equal(countBemWarning, 0)
+			assert.equal(warningCount, 0)
 		})
 
-		it('should be one error bem', () => {
-			const html = fs.readFileSync(join(__dirname, '/fixtures/no-valid-error-1.html'))
-			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
+		it("should be one error bem", () => {
+			let html = fs.readFileSync(join(__dirname, "/fixtures/no-valid-error-1.html"))
+			let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			assert.strictEqual(countBemWarning, 1)
+			assert.strictEqual(warningCount, 1)
 		})
 
-		it('should be one error bem(prefix)', () => {
-			const html = fs.readFileSync(join(__dirname, '/fixtures/prefix-error.html'))
-			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
+		it("should be one error bem(prefix)", () => {
+			let html = fs.readFileSync(join(__dirname, "/fixtures/prefix-error.html"))
+			let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			assert.strictEqual(countBemWarning, 1)
+			assert.strictEqual(warningCount, 1)
 		})
 
-		it('should be one error element-within-an-element', () => {
-			const html = fs.readFileSync(join(__dirname, '/fixtures/element-within-an-element.html'))
-			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
+		it("should be one error element-within-an-element", () => {
+			let html = fs.readFileSync(join(__dirname, "/fixtures/element-within-an-element.html"))
+			let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			assert.strictEqual(countBemWarning, 1)
+			assert.strictEqual(warningCount, 1)
 		})
 
-		it('should not be errors because a valid bem html is passed', () => {
-			const html = fs.readFileSync(join(__dirname, '/fixtures/page__elements.html'))
-			const { countBemWarning } = htmlBemlinter({ content: html.toString() })
+		it("should not be errors because a valid bem html is passed", () => {
+			let html = fs.readFileSync(join(__dirname, "/fixtures/page__elements.html"))
+			let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			assert.strictEqual(countBemWarning, 0)
+			assert.strictEqual(warningCount, 0)
 		})
 
-		it('output the success result to the console', () => {
-			const html = fs.readFileSync(join(__dirname, '/fixtures/valid.html'))
+		it("output the success result to the console", () => {
+			let html = fs.readFileSync(join(__dirname, "/fixtures/valid.html"))
 
-			htmlBemlinterResult({ name: 'valid.html', content: html.toString() })
+			htmlBemlinterResult({ name: "valid.html", content: html.toString() })
 		})
 
-		it('output the error result to the console', () => {
-			const html = fs.readFileSync(join(__dirname, '/fixtures/no-valid-error-1.html'))
+		it("output the error result to the console", () => {
+			let html = fs.readFileSync(join(__dirname, "/fixtures/no-valid-error-1.html"))
 
-			htmlBemlinterResult({ name: 'no-valid-error-1.html', content: html.toString() })
+			htmlBemlinterResult({ name: "no-valid-error-1.html", content: html.toString() })
 		})
 
-		it('output the error result to the console', () => {
-			const html = fs.readFileSync(join(__dirname, '/fixtures/element-within-an-element.html'))
+		it("output the error result to the console", () => {
+			let html = fs.readFileSync(join(__dirname, "/fixtures/element-within-an-element.html"))
 
-			htmlBemlinterResult({ name: 'element-within-an-element.html', content: html.toString() })
+			htmlBemlinterResult({ name: "element-within-an-element.html", content: html.toString() })
 		})
 	})
 })
