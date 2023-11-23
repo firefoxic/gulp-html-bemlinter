4ca9084a86be2391406e5f824b0f60c259bc495d
firefoxic <firefoxic.dev@gmail.com>
2023-11-23 18:07:31 +0300

    Replace `mocha` with `node:test`

--- test/index.js
+++ test/index.js
@@ -1,58 +1,58 @@
-import { readFileSync } from "node:fs"
 import { equal } from "node:assert/strict"
-
-import { describe, it } from "mocha"
+import { readFileSync } from "node:fs"
+import process from "node:process"
+import { describe, it } from "node:test"
 
 import { htmlBemlinter, htmlBemlinterResult } from "../src/index.js"
 
 
 describe("gulp-html-bemlinter", () => {
-	describe("Should work in buffer mode.", () => {
-		it("Should be no errors with correct bem html.", () => {
-			let html = readFileSync(new URL("./fixtures/valid.html", import.meta.url))
-			let { warningCount } = htmlBemlinter({ content: html.toString() })
+	it("Should be no errors with correct bem html.", () => {
+		let html = readFileSync(new URL("./fixtures/valid.html", import.meta.url))
+		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			equal(warningCount, 0)
-		})
+		equal(warningCount, 0)
+	})
 
-		it("Should be one error in bem: element not inside its block.", () => {
-			let html = readFileSync(new URL("./fixtures/error-in-element-without-block.html", import.meta.url))
-			let { warningCount } = htmlBemlinter({ content: html.toString() })
+	it("Should be one error in bem: element not inside its block.", () => {
+		let html = readFileSync(new URL("./fixtures/error-in-element-without-block.html", import.meta.url))
+		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			equal(warningCount, 1)
-		})
+		equal(warningCount, 1)
+	})
 
-		it("Should be two errors in bem: modifiers without modifiable entity.", () => {
-			let html = readFileSync(new URL("./fixtures/error-in-modifiers.html", import.meta.url))
-			let { warningCount } = htmlBemlinter({ content: html.toString() })
+	it("Should be two errors in bem: modifiers without modifiable entity.", () => {
+		let html = readFileSync(new URL("./fixtures/error-in-modifiers.html", import.meta.url))
+		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			equal(warningCount, 2)
-		})
+		equal(warningCount, 2)
+	})
 
-		it("Should be one error in bem: element of element.", () => {
-			let html = readFileSync(new URL("./fixtures/error-in-element-of-element.html", import.meta.url))
-			let { warningCount } = htmlBemlinter({ content: html.toString() })
+	it("Should be one error in bem: element of element.", () => {
+		let html = readFileSync(new URL("./fixtures/error-in-element-of-element.html", import.meta.url))
+		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			equal(warningCount, 1)
-		})
+		equal(warningCount, 1)
+	})
 
-		it("Should be one error in bem: invalid html should be ignored.", () => {
-			let html = readFileSync(new URL("./fixtures/error-in-markup.html", import.meta.url))
-			let { warningCount } = htmlBemlinter({ content: html.toString() })
+	it("Should be one error in bem: invalid html should be ignored.", () => {
+		let html = readFileSync(new URL("./fixtures/error-in-markup.html", import.meta.url))
+		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
-			equal(warningCount, 1)
-		})
+		equal(warningCount, 1)
+	})
 
-		it("Error output example.", () => {
-			let html = readFileSync(new URL("./fixtures/error-in-element-of-element.html", import.meta.url))
+	it("Error output example.", () => {
+		let html = readFileSync(new URL("./fixtures/error-in-element-of-element.html", import.meta.url))
 
-			htmlBemlinterResult({ name: "error-in-element-of-element.html", content: html.toString() })
-		})
+		htmlBemlinterResult({ name: "error-in-element-of-element.html", content: html.toString() })
 
-		it("Successful output example.", () => {
-			let html = readFileSync(new URL("./fixtures/valid.html", import.meta.url))
+		process.exitCode = process.exitCode === 1 ? 0 : 1
+	})
 
-			htmlBemlinterResult({ name: "valid.html", content: html.toString() })
-		})
+	it("Successful output example.", () => {
+		let html = readFileSync(new URL("./fixtures/valid.html", import.meta.url))
+
+		htmlBemlinterResult({ name: "valid.html", content: html.toString() })
 	})
 })
