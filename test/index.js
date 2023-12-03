68c7943fc15c1a3f96d713b37aec65a7260c67bd
firefoxic <firefoxic.dev@gmail.com>
2023-12-04 00:30:03 +0300

    Change eslint config

--- test/index.js
+++ test/index.js
@@ -5,53 +5,53 @@ import { describe, it } from "node:test"
 
 import { htmlBemlinter, htmlBemlinterResult } from "../lib/htmlBemLinters.js"
 
-describe("html-bem-linter", () => {
-	it("should fail markup with an incorrect BEM tree", () => {
-		let html = readFileSync(new URL("./fixtures/error-in-element-of-element.html", import.meta.url))
+describe(`html-bem-linter`, () => {
+	it(`should fail markup with an incorrect BEM tree`, () => {
+		let html = readFileSync(new URL(`./fixtures/error-in-element-of-element.html`, import.meta.url))
 
-		htmlBemlinterResult({ name: "error-in-element-of-element.html", content: html.toString() })
+		htmlBemlinterResult({ name: `error-in-element-of-element.html`, content: html.toString() })
 
 		process.exitCode = process.exitCode === 1 ? 0 : 1
 	})
 
-	it("should pass markup with a correct BEM tree", () => {
-		let html = readFileSync(new URL("./fixtures/valid.html", import.meta.url))
+	it(`should pass markup with a correct BEM tree`, () => {
+		let html = readFileSync(new URL(`./fixtures/valid.html`, import.meta.url))
 
-		htmlBemlinterResult({ name: "valid.html", content: html.toString() })
+		htmlBemlinterResult({ name: `valid.html`, content: html.toString() })
 	})
 })
 
-describe("BEM tree", () => {
-	it("should be correct", () => {
-		let html = readFileSync(new URL("./fixtures/valid.html", import.meta.url))
+describe(`BEM tree`, () => {
+	it(`should be correct`, () => {
+		let html = readFileSync(new URL(`./fixtures/valid.html`, import.meta.url))
 		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
 		equal(warningCount, 0)
 	})
 
-	it("should not contain elements outside their blocks", () => {
-		let html = readFileSync(new URL("./fixtures/error-in-element-without-block.html", import.meta.url))
+	it(`should not contain elements outside their blocks`, () => {
+		let html = readFileSync(new URL(`./fixtures/error-in-element-without-block.html`, import.meta.url))
 		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
 		equal(warningCount, 1)
 	})
 
-	it("should not conatain modifiers without modifiable entity", () => {
-		let html = readFileSync(new URL("./fixtures/error-in-modifiers.html", import.meta.url))
+	it(`should not conatain modifiers without modifiable entity`, () => {
+		let html = readFileSync(new URL(`./fixtures/error-in-modifiers.html`, import.meta.url))
 		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
 		equal(warningCount, 2)
 	})
 
-	it("should contain elements of elements", () => {
-		let html = readFileSync(new URL("./fixtures/error-in-element-of-element.html", import.meta.url))
+	it(`should contain elements of elements`, () => {
+		let html = readFileSync(new URL(`./fixtures/error-in-element-of-element.html`, import.meta.url))
 		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
 		equal(warningCount, 1)
 	})
 
-	it("should not contain mixes of elements with their blocks", () => {
-		let html = readFileSync(new URL("./fixtures/error-in-element-mixed-with-block.html", import.meta.url))
+	it(`should not contain mixes of elements with their blocks`, () => {
+		let html = readFileSync(new URL(`./fixtures/error-in-element-mixed-with-block.html`, import.meta.url))
 		let { warningCount } = htmlBemlinter({ content: html.toString() })
 
 		equal(warningCount, 1)
