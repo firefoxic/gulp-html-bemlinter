3f92d3719bf48c86b2238f44236e044578473590
firefoxic <firefoxic.dev@gmail.com>
2024-03-30 13:36:09 +0300

    Add tests for `generateAsciiTree`

---
+++ test/generateAnsiTree.js
@@ -0,0 +1,67 @@
+import { describe, it } from "node:test"
+import { equal } from "node:assert/strict"
+
+import { generateAsciiTree } from "../lib/generateAsciiTree.js"
+
+describe(`generateAsciiTree`, () => {
+	it(`should return empty string for empty AST`, () => {
+		const ast = {}
+		const options = {}
+		const result = generateAsciiTree(ast, options)
+		equal(result, ``)
+	})
+
+	it(`should return ASCII representation of single node AST`, () => {
+		const ast = {
+			label: `Node 1`,
+			nodes: [],
+		}
+		const options = {}
+		const result = generateAsciiTree(ast, options)
+		equal(result, `Node 1`)
+	})
+
+	it(`should return correct ASCII tree representation for complex AST`, () => {
+		const ast = {
+			label: `Root`,
+			nodes: [
+				{
+					label: `Node 1`,
+					nodes: [
+						{
+							label: `Node 1.1`,
+							nodes: [],
+						},
+						{
+							label: `Node 1.2`,
+							nodes: [
+								{
+									label: `Node 1.2.1`,
+									nodes: [],
+								},
+								{
+									label: `Node 1.2.2`,
+									nodes: [],
+								},
+							],
+						},
+					],
+				},
+				{
+					label: `Node 2`,
+					nodes: [],
+				},
+			],
+		}
+		const options = {}
+		const result = generateAsciiTree(ast, options)
+		equal(result, `Root
+├─ Node 1
+│  ├─ Node 1.1
+│  └─ Node 1.2
+│     ├─ Node 1.2.1
+│     └─ Node 1.2.2
+└─ Node 2`)
+	})
+})
+
